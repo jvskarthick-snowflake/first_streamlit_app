28f53c347878f3ad45200de3e37a9b592c1bc648
jvskarthik.snowflake <116317804+jvskarthick-snowflake@users.noreply.github.com>
2022-10-20 17:39:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,6 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach  &  Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Biled  Free-Range Egg')
 
-Import Pandas
+import Pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
