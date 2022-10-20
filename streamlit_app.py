4f6436b204f15291163152105f8c9c7c577f663a
jvskarthik.snowflake <116317804+jvskarthick-snowflake@users.noreply.github.com>
2022-10-20 17:36:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,18 @@
 import streamlit
 
-streamlit.title('🥣 My Parents new Healthy Dinner')
+streamlit.title('🥣 My Parents new Healthy Breakfast/Lunch/Dinner')
 
 streamlit.header('🥗 Breakfast Menu 🥗')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach  &  Rocket Smoothie')
-streamlit.text('Hard-Biled  Free-Range Egg')
+streamlit.text('🥑🍞Hard-Biled  Free-Range Egg')
 
 streamlit.header('🥗 Lunch Menu 🥗')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach  &  Rocket Smoothie')
-streamlit.text('Hard-Biled  Free-Range Egg')
+streamlit.text('🥑🍞Hard-Biled  Free-Range Egg')
+
+streamlit.header('🥗 Dinner Menu 🥗')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach  &  Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Biled  Free-Range Egg')
