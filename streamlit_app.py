30da4aa800613cee40c30a1961d40dab19303212
jvskarthik.snowflake <116317804+jvskarthick-snowflake@users.noreply.github.com>
2022-10-20 17:27:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 Import steamlit
-streamlit.title('My Parents new Healthy Dinner')
-streamlit.header('Breakfast Menu')
+streamlit.title('🥣 My Parents new Healthy Dinner')
+streamlit.header('🥗 Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach  &  Rocket Smoothie')
 streamlit.text('Hard-Biled  Free-Range Egg')
