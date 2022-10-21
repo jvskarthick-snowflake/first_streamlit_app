958010d21a149f50faae91b09eca900dfe602db4
jvskarthik.snowflake <116317804+jvskarthick-snowflake@users.noreply.github.com>
2022-10-21 17:34:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ streamlit.header('🥗 Dinner Menu 🥗')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach  &  Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Biled  Free-Range Egg')
-
+streamlit.text('🥑🍞Rice')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
