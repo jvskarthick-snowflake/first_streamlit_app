1e9b29be1cdf9404bd4b268793ec3cd45e4f8763
jvskarthik.snowflake <116317804+jvskarthick-snowflake@users.noreply.github.com>
2022-10-24 17:09:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+streamlit.header('fruityvice fruit Advice')
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+
 
 
